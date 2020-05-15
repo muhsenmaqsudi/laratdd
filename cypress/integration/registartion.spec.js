fe7dc58eb0e92e89e68765b18e01c153f68c30b2
muhsenmaqsudi <muhsenmaqsudi@gmail.com>
2020-05-15 16:08:36 +0430

    added laravel auth registration cypress test

---
+++ cypress/integration/registartion.spec.js
@@ -0,0 +1,18 @@
+import faker from 'faker';
+
+describe('Registration', () => {
+    const email = faker.internet.email();
+    const password = faker.internet.password();
+
+    it('should successfully registering', function () {
+        cy.visit('http://127.0.0.1:8000/register');
+        cy.get('input[name=name]').type(faker.name.findName());
+        cy.get('input[name=email]').type(email);
+        cy.get('input[name=password]').type(password);
+        cy.get('input[name=password_confirmation]').type(password);
+        cy.get('button').contains('Register').click();
+        cy.url().should('contain', '/home');
+        cy.get('#navbarDropdown').click();
+        cy.contains('Logout').click();
+    });
+});
